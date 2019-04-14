6576ceb856d34aa991110c63f524860f04442d0e
Dominik <dominiug@yahoo.com>
2019-04-15 00:18:41 +0200

    sql updates
    - formatted code (uncommented sql commands)

--- SQL/database_initialisation.sql
+++ SQL/database_initialisation.sql
@@ -1,143 +1,143 @@
 -- /// Create tables for managing accounts
 ----------------------------------------
--- create table AccountTypes(
---   id integer primary key identity,
---   type varchar(40),
--- );
+create table AccountTypes(
+  id integer primary key identity,
+  type varchar(40),
+);
 
--- create table SystemUsers(
---   id integer primary key identity,
---   login varchar(40),
---   passwd varchar(40),
---   name varchar(40),
---   surname varchar(40),
---   accountTypeId integer,
---   FOREIGN KEY (accountTypeId) REFERENCES AccountTypes(id)
--- );
+create table SystemUsers(
+  id integer primary key identity,
+  login varchar(40),
+  passwd varchar(40),
+  name varchar(40),
+  surname varchar(40),
+  accountTypeId integer,
+  FOREIGN KEY (accountTypeId) REFERENCES AccountTypes(id)
+);
 
 
 -- /// Insert 1 admin and 1 user
 ----------------------------------------
--- insert into AccountTypes values
---   ('admin'),
---   ('user')
--- go
+insert into AccountTypes values
+  ('admin'),
+  ('user')
+go
 
--- insert into SystemUsers values
--- ('admin1', 'admin1','Dominik','Gomez',1)
--- go
+insert into SystemUsers values
+('admin1', 'admin1','Dominik','Gomez',1)
+go
 
--- insert into SystemUsers values
--- ('user1', 'user1','Julia','Denis',2)
--- go
+insert into SystemUsers values
+('user1', 'user1','Julia','Denis',2)
+go
 
 -- /// Create table for LogInfo
 ----------------------------------------
--- create table LogInfo(
---   logId integer primary key identity,
---   logDate datetime,
---   userId integer,
---   FOREIGN KEY (userId) REFERENCES SystemUsers(id)
--- );
+create table LogInfo(
+  logId integer primary key identity,
+  logDate datetime,
+  userId integer,
+  FOREIGN KEY (userId) REFERENCES SystemUsers(id)
+);
 
 -- /// Create tables for managing items
 ----------------------------------------
 
--- create table ItemTypes(
---   id integer primary key identity,
---   type varchar(40),
--- );
+create table ItemTypes(
+  id integer primary key identity,
+  type varchar(40),
+);
 
--- insert into ItemTypes values
--- ('book'),
--- ('magazine'),
--- ('film')
--- go
+insert into ItemTypes values
+('book'),
+('magazine'),
+('film')
+go
 
--- create table Items(
---   id integer primary key identity,
---   typeId integer,
---   itemId integer,
---   FOREIGN KEY (typeId) REFERENCES ItemTypes(id)
--- );
+create table Items(
+  id integer primary key identity,
+  typeId integer,
+  itemId integer,
+  FOREIGN KEY (typeId) REFERENCES ItemTypes(id)
+);
 
 -- /// Insert 1 book, 1 magazine and 1 film into [Items] table
 ----------------------------------------
--- insert into Items values
--- (1),
--- (2),
--- (3)
--- go
+insert into Items values
+(1),
+(2),
+(3)
+go
 
 -- /// Create tables for Authors, Publishers, Directors, Genres
 ----------------------------------------
--- create table Authors(
---   id integer primary key identity,
---   name varchar(40),
---   surname varchar(40)
--- );
+create table Authors(
+  id integer primary key identity,
+  name varchar(40),
+  surname varchar(40)
+);
 
--- create table Publishers(
---   id integer primary key identity,
---   name varchar(40)
--- );
+create table Publishers(
+  id integer primary key identity,
+  name varchar(40)
+);
 
--- create table Directors(
---   id integer primary key identity,
---   name varchar(40),
---   surname varchar(40)
--- );
+create table Directors(
+  id integer primary key identity,
+  name varchar(40),
+  surname varchar(40)
+);
 
--- create table Genres(
---   id integer primary key identity,
---   genre varchar(40)
--- );
+create table Genres(
+  id integer primary key identity,
+  genre varchar(40)
+);
 
 -- /// Create tables for Books, Magazines, Films
 ----------------------------------------
--- create table Books(
---   id integer primary key identity,
---   title varchar(40),
---   authorId integer,
---   edition integer,
---   year integer,
---   publisherId integer,
---   FOREIGN KEY (authorId) REFERENCES Authors(id),
---   FOREIGN KEY (publisherId) REFERENCES Publishers(id)
--- );
---
--- create table Magazines(
---   id integer primary key identity,
---   title varchar(40),
---   year integer,
---   issue integer,
---   publisherId integer,
---   FOREIGN KEY (publisherId) REFERENCES Publishers(id)
--- );
+create table Books(
+  id integer primary key identity,
+  title varchar(40),
+  authorId integer,
+  edition integer,
+  year integer,
+  publisherId integer,
+  FOREIGN KEY (authorId) REFERENCES Authors(id),
+  FOREIGN KEY (publisherId) REFERENCES Publishers(id)
+);
 
--- create table Films(
---   id integer primary key identity,
---   title varchar(40),
---   directorId integer,
---   year integer,
---   genreId integer,
---   FOREIGN KEY (directorId) REFERENCES Directors(id),
---   FOREIGN KEY (genreId) REFERENCES Genres(id)
--- );
+create table Magazines(
+  id integer primary key identity,
+  title varchar(40),
+  year integer,
+  issue integer,
+  publisherId integer,
+  FOREIGN KEY (publisherId) REFERENCES Publishers(id)
+);
+
+create table Films(
+  id integer primary key identity,
+  title varchar(40),
+  directorId integer,
+  year integer,
+  genreId integer,
+  FOREIGN KEY (directorId) REFERENCES Directors(id),
+  FOREIGN KEY (genreId) REFERENCES Genres(id)
+);
 
 -- /// Insert genres, publishers
 ----------------------------------------
--- insert into Genres values
--- ('action'),
--- ('comedy'),
--- ('horror'),
--- ('scifi')
--- go
+insert into Genres values
+('action'),
+('comedy'),
+('horror'),
+('scifi')
+go
 
--- insert into Publishers values
--- ('Warsaw Books'),
--- ('Claroscuro Publishing House'),
--- ('Agora'),
--- ('Science Publishers')
--- go
+insert into Publishers values
+('Warsaw Books'),
+('Claroscuro Publishing House'),
+('Agora'),
+('Science Publishers')
+go
 
